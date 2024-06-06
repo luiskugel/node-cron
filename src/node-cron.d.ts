1a4aad4cf68e24910196c2ba55ff8bcc6a3626f5
Luis Kugel <luis.kugel@idot-digital.de>
2024-06-06 17:21:12 +0200

    Fixed types to also have added function

--- src/node-cron.d.ts
+++ src/node-cron.d.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from "events";
+import { EventEmitter } from 'events';
 
 /**
  * Creates a new task to execute the given function when the cron expression ticks.
@@ -7,9 +7,9 @@ import { EventEmitter } from "events";
  * @param options
  */
 export function schedule(
-    cronExpression: string,
-    func: ((now: Date | "manual" | "init") => void) | string,
-    options?: ScheduleOptions,
+  cronExpression: string,
+  func: ((now: Date | 'manual' | 'init') => void) | string,
+  options?: ScheduleOptions
 ): ScheduledTask;
 
 /**
@@ -24,34 +24,35 @@ export function validate(cronExpression: string): boolean;
 export function getTasks(): Map<string, ScheduledTask>;
 
 export interface ScheduledTask extends EventEmitter {
-    now: (now?: Date) => void;
-    start: () => void;
-    stop: () => void;
+  now: (now?: Date) => void;
+  start: () => void;
+  stop: () => void;
+  remove: () => void;
 }
 
 export interface ScheduleOptions {
-    /**
-     * A boolean to set if the created task is scheduled.
-     *
-     * Defaults to `true`
-     */
-    scheduled?: boolean | undefined;
-    /**
-     * The timezone that is used for job scheduling
-     */
-    timezone?: string;
-    /**
-     * Specifies whether to recover missed executions instead of skipping them.
-     *
-     * Defaults to `false`
-     */
-    recoverMissedExecutions?: boolean;
-    /**
-     * The schedule name
-     */
-    name?: string;
-    /**
-     * Execute task immediately after creation
-     */
-    runOnInit?: boolean;
+  /**
+   * A boolean to set if the created task is scheduled.
+   *
+   * Defaults to `true`
+   */
+  scheduled?: boolean | undefined;
+  /**
+   * The timezone that is used for job scheduling
+   */
+  timezone?: string;
+  /**
+   * Specifies whether to recover missed executions instead of skipping them.
+   *
+   * Defaults to `false`
+   */
+  recoverMissedExecutions?: boolean;
+  /**
+   * The schedule name
+   */
+  name?: string;
+  /**
+   * Execute task immediately after creation
+   */
+  runOnInit?: boolean;
 }
